85a81f60a18309dcb293749ff0586cc72af902e4
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-05-03 16:23:47 +0200

    added getDetails andre

--- js/browse.js
+++ js/browse.js
@@ -5,6 +5,14 @@ const baseUrl = "https://api.noroff.dev/api/v1";
 const allEndpoints = "/rainy-days";
 const specificEndpoint = "/rainy-days/<id>";
 
+// Get detail om en
+async function getDetails(id) {
+  const response = await fetch(baseUrl + `/rainy-days/${id}`);
+  const data = await response.json();
+  console.log({ data });
+  return data;
+}
+
 async function fetchData() {
   const response = await fetch(baseUrl + allEndpoints);
   const data = await response.json();
@@ -20,14 +28,37 @@ async function renderHTML() {
   jacketList.innerHTML = ``;
   //
   productlist.forEach(function (element) {
+    // console.log hva et element inneholder. Ser for meg at den inneholder en id av noe slag som man kan hente informasjon om
+    const myFunction = () => {
+      window.location.href = `details/${element.id}`;
+    };
+
+    const lol = () => console.log("trykker på meg");
+
+    // På li så må det legges på en id og en onClick. Så enten så kan man sette id på li til å være lik id på element
+    // også må man lage en sånn document.getElementById("id på element").addEventListener("click", myFunction);
+    // jeg er usikker på om man kan skrive <li onclick="myFunction()"> ? Det hadde vært det enkleste.
     jacketList.innerHTML += `
-    <li>
+    <li id=${element.id} onclick="lol()">
           <div>
             <h2>${element.title}</h2>
             <img src="${element.image}" alt="#" />
             <p>${element.description}</p>
           </div>
         </li>`;
+
+    document.title = "Jacket details"; // Endrer title på siden
+    // const liElement = document.getElementById(element.id);
+
+    // liElement.addEventListener("click", function () {
+    //   window.location.href = `details/${element.id}`;
+    // });
   });
 }
 renderHTML();
+
+// const liElement = document.querySelector("li");
+
+// liElement.addEventListener("click", function () {
+//   window.location.href = `details/${element.id}`;
+// });
